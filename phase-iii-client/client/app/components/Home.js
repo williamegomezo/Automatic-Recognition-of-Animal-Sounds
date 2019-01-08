1c164f47bc5c0f0c5e449d405c857dccad1ec8ca
William Gomez <wgomez@hugeinc.com>
2019-01-08 00:59:33 -0500

    Scss working.

--- phase-iii-client/client/app/components/Home.js
+++ phase-iii-client/client/app/components/Home.js
@@ -1,23 +1,66 @@
 // @flow
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
-import routes from '../constants/routes';
-import styles from './Home.css';
+import Grid from '@material-ui/core/Grid';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import FolderIcon from '@material-ui/icons/Folder';
+import { remote } from 'electron';
 
-type Props = {};
-
-export default class Home extends Component<Props> {
-  props: Props;
+export default class Home extends Component {
+  selectDirectory = () => {
+    remote.dialog.showOpenDialog(
+      {
+        properties: ['openDirectory']
+      },
+      filePaths => {
+        console.log(filePaths);
+      }
+    );
+  };
 
   render() {
     return (
-      <div className={styles.container} data-tid="container">
-        <h2>Home</h2>
-        <Button variant="contained" color="primary">
+      <div data-tid="container">
+        <h2>Aureas</h2>
+        <div className="row">
+          <div className="col-xs-6">Hola</div>
+          <div className="col-xs-6">Super</div>
+        </div>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={this.selectDirectory}
+        >
           Hello World
         </Button>
-        <Link to={routes.COUNTER}>to Counter</Link>
+
+        <Grid item xs={12} md={6}>
+          <div>
+            <List className="fileList">
+              <ListItem>
+                <ListItemIcon>
+                  <FolderIcon />
+                </ListItemIcon>
+                <ListItemText primary="Single-line item" />
+              </ListItem>
+              <ListItem>
+                <ListItemIcon>
+                  <FolderIcon />
+                </ListItemIcon>
+                <ListItemText primary="Single-line item" />
+              </ListItem>
+              <ListItem>
+                <ListItemIcon>
+                  <FolderIcon />
+                </ListItemIcon>
+                <ListItemText primary="Single-line item" />
+              </ListItem>
+            </List>
+          </div>
+        </Grid>
       </div>
     );
   }
